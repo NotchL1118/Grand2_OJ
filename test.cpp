d5d0f479f6d228d109d56285d6550b5ff89bd78d
NotchL1118 <lsy358027330@163.com>
2022-04-11 11:11:40 +0800

    master

--- test.cpp
+++ test.cpp
@@ -3,32 +3,7 @@
 #include <cstdio>
 using namespace std;
 
-const int N = 10010;
-
-int n;
-int g[N];
-int res;
 int main()
 {
-	cin >> n;
-	
-	for(int i = 0; i < n; i ++ )
-		cin >> g[N];
-	
-	for(int i = 0; i < n; i ++ ) 
-	{
-		int minv = 1e9, maxv = -1e9;
-		for(int j = i; j < n; j ++ )
-		{
-			minv = min(minv,g[j]);
-			maxv = max(maxv,g[j]);
-			if(maxv - minv == j - i)
-			{
-				cout << minv << ' ' << maxv <<endl;
-				res++;
-			} 
-		}		
-	}
-	cout << res;
-	return 0;
+	cout << "Hello World" << endl;
 }
