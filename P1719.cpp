e8558f0d5ee5c632086404af132f4b6daa2b71c2
NotchL1118 <lsy358027330@163.com>
2022-05-26 11:17:09 +0800

    TestClash

--- P1719.cpp
+++ P1719.cpp
@@ -3,37 +3,10 @@
 #include <algorithm>
 using namespace std;
 
-const int N = 1e6 + 10;
-
-int n,k;
-int a[N],place[N];
-
 int main()
 {
-    scanf("%d%d",&n,&k);
-    int num = 0;
-    for(int i = 1; i <= n; i ++ )
-    {
-        scanf("%d",&a[i]);
-        if(a[i] == k)
-            place[++num] = i;
-    }
 
-    int ans = 0;
-    for(int i = num; i >= 1; i -- )
-    {
-        if(place[i] % 2 == 0 || place[i] == 1) ans++; //偶数位置
-        else    //奇数位置
-        {
-            int origin = i; //原位置
-            while(i >= 1 && place[i] %2 != 0)
-            {
-                if(place[i] == 1) break;
-               i --; //找到原位置前第一个偶数位置
-            }
-            ans += origin - i + 1;
-        }
-    }
-    cout << ans;
+
+    
     return 0;
 }
